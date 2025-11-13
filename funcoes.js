469ecdfff0cb2490967a70a7b5a82898e849c546
Aparecidalml <aparecidalml0@gmail.com>
2025-11-13 12:15:58 +0000

    verificação do login

---
+++ funcoes.js
@@ -0,0 +1,11 @@
+function fazerlogin(){
+    let login = document.getElementById('login').value
+    let senha = document.getElementById('senha').value
+
+    if(login === 'cenep' && senha === 'cenep123'){
+        window.location.href = 'pagina2.html'
+    }else{
+        alert('Login ou senha Incorretos')
+    }
+
+}
