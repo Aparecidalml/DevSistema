d4d34eaa8959d85be59712d9b886d6c14a28739e
Aparecidalml <aparecidalml0@gmail.com>
2025-11-13 13:38:34 +0000

    pagina2

--- funcoes.js
+++ funcoes.js
@@ -3,9 +3,15 @@ function fazerlogin(){
     let senha = document.getElementById('senha').value
 
     if(login === 'cenep' && senha === 'cenep123'){
+        alert('Login e senha Corretos!')
+        localStorage.setItem('nome', login)
         window.location.href = 'pagina2.html'
     }else{
-        alert('Login ou senha Incorretos')
+        alert('Login ou senha Incorreto!')
     }
 
 }
+
+document.getElementById('titulo').innerHTML += 
+    localStorage.getItem('titulo')
+
